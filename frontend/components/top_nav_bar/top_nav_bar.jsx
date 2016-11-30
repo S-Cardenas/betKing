e5b51b026b99c48f8de3aefae6beff29339a95f1
stefan <fets809@gmail.com>
2016-11-30 18:05:47 -0500

    added betking logo to nav bar

---
+++ frontend/components/top_nav_bar/top_nav_bar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class TopNavBar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse sample">
+        <div className="container-fluid">
+          <div className="navbar-header sample">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">
+              <img className="img-responsive" src="assets/images/logo.png" alt="" style={{width: 167.4, height: 46.2}}/>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#">Page 1</a></li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default TopNavBar;
---
+++ frontend/entry.jsx
@@ -0,0 +1,10 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Import
+import TopNavBar from './components/top_nav_bar/top_nav_bar';
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(<TopNavBar />, document.getElementById('root'));
+});
